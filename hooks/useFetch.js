9eff5c2b0eda08710369e322b12df2c7e14b24d7
unknown <sadekirfan3@gmail.com>
2021-06-27 23:53:16 +0600

    query fixed

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,25 +1,45 @@
 import { useState, useEffect, useCallback } from "react";
 
-const useFetch = (query = "", offset) => {
+const useFetch = (query, offset) => {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 	const [list, setList] = useState([]);
+	const [checkQuery, setCheckQuery] = useState("");
+	const [queryChanged, setQueryChanged] = useState(false);
 
-	const sendQuery = useCallback(async () => {
-		try {
-			setLoading(true);
-			setError(false);
-			const res = await fetch(
-				`https://api.giphy.com/v1/gifs/search?api_key=mvca6YkMyvd1PYBJRiIYzFJwRNpTBhaK&q=${query}&limit=12&offset=${offset}&rating=pg-13&lang=en`
-			);
-			const data = await res.json();
-			const previewList = await data.data.map((gif) => gif.images.preview.mp4);
-			setList((pre) => [...pre, ...previewList]);
-			setLoading(false);
-		} catch (err) {
-			setError(err);
+	const sendQuery = useCallback(
+		async (query) => {
+			try {
+				setLoading(true);
+				setError(false);
+				const res = await fetch(
+					`https://api.giphy.com/v1/gifs/search?api_key=mvca6YkMyvd1PYBJRiIYzFJwRNpTBhaK&q=${query}&limit=12&offset=${offset}&rating=pg-13&lang=en`
+				);
+				const data = await res.json();
+				const previewList = await data.data.map(
+					(gif) => gif.images.preview.mp4
+				);
+				setList((pre) => [...pre, ...previewList]);
+				setLoading(false);
+			} catch (err) {
+				setError(err);
+			}
+		},
+		[query, offset]
+	);
+
+	useEffect(() => {
+		if (checkQuery !== query) {
+			setQueryChanged(true);
+			setCheckQuery(query);
+		} else {
+			setQueryChanged(false);
 		}
-	}, [query, offset]);
+
+		if (queryChanged) {
+			setList([]);
+		}
+	}, [query]);
 
 	useEffect(() => {
 		sendQuery(query);
